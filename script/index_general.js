daddc7766cf6dee5a16c3f9906f18ab87c7fc73c
ankitkataria7890 <58991431+ankitkataria7890@users.noreply.github.com>
2020-06-27 20:02:05 +0530

    Add files via upload

---
+++ script/index_general.js
@@ -0,0 +1,42 @@
+window.onscroll = function() {index(),left()};
+var navbar = document.getElementsByClassName("index")[0];
+var sticky = navbar.offsetTop;
+
+function index() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+<!--var leftbar = document.getElementById("left");
+var leftsticky = leftbar.offsetTop;
+
+function left() {
+  if (window.pageYOffset >= leftsticky) {
+    leftbar.classList.add("leftsticky")
+  } else {
+    leftbar.classList.remove("leftsticky");
+  }
+}
+-->
+function backhome() {
+document.body.scrollIntoView({behavior: 'smooth', block: 'start'});
+}
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+
+  document.getElementById("close").style.display = "block";
+}
+
+function footer(){
+  var x=document.getElementsByClassName("footer")[0];
+ x.style.display="block";
+}
+
+function footergo(){
+  document.getElementsByClassName("footer")[0].style.display = "none";
+}
