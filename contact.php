abf5092ab5c67011ba276943f33012b2e6a9b04b
ankitkataria7890 <58991431+ankitkataria7890@users.noreply.github.com>
2020-06-28 23:59:11 +0530

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,30 @@
+<DOCTYPE! html>
+<html>
+<head>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<meta name="viewport" content="width=device-width,
+<title>Contact</title>
+<style>
+body{background-color:rgba(0,0,225,0.6);
+}
+center{
+color:white;
+margin:auto;
+margin-top:15%;}
+.material-icons{
+font-size:36px;
+}
+</style>
+</head>
+<body>
+<center>
+<h1>Coming Soon...</h1>
+<h6>Till You Can Follow us ON</h6>
+
+<a href="#"><dfn title="facebook"><img src="https://img.icons8.com/bubbles/50/000000/facebook-new.png"/></dfn></a>&nbsp;&nbsp;
+<a href="#"><dfn title="instagram"><img src="https://img.icons8.com/bubbles/48/000000/instagram-new.png"/></dfn></a>&nbsp;&nbsp;
+<a href="#"><dfn title="linkden"><img src="https://img.icons8.com/bubbles/48/000000/linkedin.png"/></dfn></a>&nbsp;&nbsp;
+<a href="#"><dfn title="twitter"><img src="https://img.icons8.com/bubbles/48/000000/twitter.png"/></dfn></a>&nbsp;&nbsp;
+</center>
+</body>
+<html>
