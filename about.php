11b2907ccbb4db4b87ac71e7a76624556e794d8a
ankitkataria7890 <58991431+ankitkataria7890@users.noreply.github.com>
2021-05-29 00:42:51 +0530

    Update about.php

--- about.php
+++ about.php
@@ -1,4 +1,133 @@
+
 <DOCTYPE! html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/about.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+<title>About Us</title>
+<style>
+ 
+/* .row:after {
+  content: "";
+  display: table;
+  clear: both;
+} */
+ </style>
+</head>
+<body>
+
+ <img src="gallery/logo1.jpg" width="100px" height="80px" alt="logo" style="background:transparent">
+<div id="one" >
+  <center ><br><br>
+  <p>Way to world of indite <br> write what you can think </p>
+ <h3>Unlock the mind and hands </h3>
+<button id="start" onclick="location.href='login.php'"><b>Create your post</b></button><br><br>
+    </center>
+</div>
+ <center>
+<div id="two" >
+            <div class="column">
+            <img id="secure" src="gif/security.gif" alt="secure" style="width:auto;height:22em" > 
+         <!-- </span> -->
+        </div>
+        
+          <span class="dropdown" style="margin-left: -15em;margin-right: 1em" >
+            <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+              Menu
+            </button>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="#">Link 1</a>
+              <a class="dropdown-item" href="#">Link 2</a>
+              <a class="dropdown-item" href="#">Link 3</a>
+            </div>
+        </span>
+          <span class="dropdown" >
+            <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+              Menu
+            </button>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="#">Link 1</a>
+              <a class="dropdown-item" href="#">Link 2</a>
+              <a class="dropdown-item" href="#">Link 3</a>
+            </div>
+        </span>
+ 
+</div>
+</center>
+<div id="simple1" >
+<div class="parallax1">
+<div class="caption">
+<span class="border">Use simple but tempting words</span>
+</div>
+</div></div>
+<div class="textparallax" >
+  <center> <h1>About</h1></center>
+ To utilise the time during covid-19 when all we were locked at home . Started on working qtsindite.herokuapp.com , With the time we go so far that now we are presenting to you all our hardwork and time that we spent the whole day and night . It is a simple project that will allow you to post ,like, comments, write blogs and much more in coming versions or upgrade .
+ so, click on create your post and explore it . Thankyou :')
+</div>
+<div class="parallax2">
+ <div class="caption">
+  <span  style="background-color:transparent;padding:18px;letter-spacing:10px;font-size:25px;color:white;">LESS HEIGHT</span>
+  </div></div>
+<div  class="textparallax">
+<center>
+Shorter contents always attracts to more readers.
+Stand out with various background color and 
+designed fonts.
+</center>
+</div>
+<div class="parallax3"></div>
+<div  class="textparallax">
+<center>
+Caputred your moments and show them to your very close ones . Build your attractive POST/BLOG and showcase.
+</center>
+</div>
+<div id="simple2">
+<div class="parallax4" >
+<div class="caption">
+<span class="border">Move Ahead</span>
+</div>
+</div>
+</div>
+<!--div id="four"  -->
+<div class="grid">
+<div><center>
+<img id="share" src="gif/share.gif"   style="opacity:0.7" alt="share"><br>
+<h1 id="giphy"> Share </h1>
+<span style="color:rgb(124, 146, 86);">Share your collection or post with your friends
+on popular platforms without any hesitation. </span>
+</center>
+</div>
+<div><center>
+<img id="secure" src="gif/security.gif" alt="secure" > 
+<h1 id="giphy"> Security </h1>
+<span style="color:rgb(124, 146, 86);" >Your content or Data is completely secure with Qts security so , write share without worry . </span>
+</center>
+</div>
+</div>
+<div id="follow" > 
+<center>
+Join us<br><br>
+<button id="start"   onclick="location.href='login.php'" style="border-radius:30px;"><b>Get Started</b></button><br><br><br>
+ Follow Us On
+<br>
+<a href="#"><dfn title="facebook"><img src="https://img.icons8.com/bubbles/50/000000/facebook-new.png" alt="facebook"/></dfn></a>&nbsp;&nbsp;
+<a href="#"><dfn title="instagram"><img src="https://img.icons8.com/bubbles/48/000000/instagram-new.png" alt="instagram"/></dfn></a>&nbsp;&nbsp;
+<a href="#"><dfn title="linkden"><img src="https://img.icons8.com/bubbles/48/000000/linkedin.png" alt="linkedin"/></dfn></a>&nbsp;&nbsp;
+<a href="#"><dfn title="twitter"><img src="https://img.icons8.com/bubbles/48/000000/twitter.png" alt="twitter"/></dfn></a>&nbsp;&nbsp;
+<br><br><br><br><br><span style="font-size:12px; font-weight:400;color:rgba(13, 26, 38,0.5);">Disclaimer: Legal information is not legal advice , <a href="disclaimer.php"style="color:rgb(153, 204, 255)">Read the disclaimer.</a><br>
+<span style="font-size:14px;line-height:200%;font-weight:400; color:rgba(13, 26, 38,0.5);"> Copyright c 2020 QTS XYZ, Inc. All rights reserverd. <a href="termsofuse.php" style="color:rgb(153, 204, 255)"> Terms of Use </a>|<a href="privacypolicy.php" style="color:rgb(153, 204, 255)"> Privacy Policy - REVISED</a></span>
+<br><br> <a href="aboutus.php">About Us | Team </a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="user_blog.php">Blog </a>
+</center>
+</div>
+</body>
+</html>
+<!-- <DOCTYPE! html>
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="css/about.css">
@@ -152,4 +281,4 @@ Join us<br><br>
 </center>
 </div>
 </body>
-</html>
+</html> -->
